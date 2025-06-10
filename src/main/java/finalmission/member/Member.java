b5caa4cecd66afe2e98c6df2f0d6468f04ad4888
egaeng09 <egaeng09@gmail.com>
2025-06-10 13:54:04 +0900

    feat: 엔티티 정의

---
+++ src/main/java/finalmission/member/Member.java
@@ -0,0 +1,18 @@
+package finalmission.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+}
---
+++ src/main/java/finalmission/seat/Seat.java
@@ -0,0 +1,23 @@
+package finalmission.seat;
+
+import finalmission.venue.Venue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer seatNumber;
+
+    @ManyToOne
+    private Venue venue;
+}
