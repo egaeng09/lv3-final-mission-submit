0e0e6c2ade20a8b6794b4523354a39076cb7566b
egaeng09 <egaeng09@gmail.com>
2025-06-10 15:14:08 +0900

    feat: 공연장 추가 및 조회 기능 구현

---
+++ src/main/java/finalmission/venue/service/detail/VenueQueryService.java
@@ -0,0 +1,23 @@
+package finalmission.venue.service.detail;
+
+import finalmission.venue.domain.Venue;
+import finalmission.venue.repository.VenueRepository;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class VenueQueryService {
+
+    private final VenueRepository venueRepository;
+
+    public Venue get(Long id) {
+        return venueRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("ID에 해당하는 공연장을 찾을 수 없습니다."));
+    }
+
+    public List<Venue> getAll() {
+        return venueRepository.findAll();
+    }
+}
