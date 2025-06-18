7d93b9006012f2be90b3a5fb314d18e338306c7a
egaeng09 <egaeng09@gmail.com>
2025-06-18 16:59:39 +0900

    refactor: 토큰 추출 시 예외 레벨 변경

--- src/main/java/finalmission/member/auth/util/JwtExtractor.java
+++ src/main/java/finalmission/member/auth/util/JwtExtractor.java
@@ -1,6 +1,7 @@
 package finalmission.member.auth.util;
 
 import finalmission.common.exception.AuthenticationException;
+import finalmission.common.exception.AuthorizationException;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.JwtException;
@@ -24,7 +25,7 @@ public class JwtExtractor {
 
     public String extractToken(Cookie[] cookies) {
         if (cookies == null) {
-            throw new AuthenticationException("쿠키가 존재하지 않습니다.");
+            throw new AuthorizationException("쿠키가 존재하지 않습니다.");
         }
 
         for (Cookie cookie : cookies) {
@@ -33,7 +34,7 @@ public class JwtExtractor {
             }
         }
 
-        throw new AuthenticationException("쿠키에 Token 정보가 없습니다.");
+        throw new AuthorizationException("쿠키에 Token 정보가 없습니다.");
     }
 
     private Claims extractClaims(String token) {
@@ -44,11 +45,11 @@ public class JwtExtractor {
                     .parseClaimsJws(token)
                     .getBody();
         } catch (ExpiredJwtException e) {
-            throw new AuthenticationException("만료된 토큰입니다.");
+            throw new AuthorizationException("만료된 토큰입니다.");
         } catch (MalformedJwtException e) {
-            throw new AuthenticationException("잘못된 토큰입니다.");
+            throw new AuthorizationException("잘못된 토큰입니다.");
         } catch (JwtException e) {
-            throw new AuthenticationException("유효하지 않은 토큰입니다.");
+            throw new AuthorizationException("유효하지 않은 토큰입니다.");
         }
     }
 }
