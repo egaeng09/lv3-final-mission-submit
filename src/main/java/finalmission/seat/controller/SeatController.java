5aeec82172ccdb4027830495318c5aea8f7d4805
egaeng09 <egaeng09@gmail.com>
2025-06-18 17:44:12 +0900

    feat: 예약 여부를 포함한 좌석 조회 기능 추가

--- src/main/java/finalmission/seat/controller/SeatController.java
+++ src/main/java/finalmission/seat/controller/SeatController.java
@@ -2,6 +2,7 @@ package finalmission.seat.controller;
 
 import finalmission.seat.controller.dto.SeatRequest;
 import finalmission.seat.controller.dto.SeatResponse;
+import finalmission.seat.repository.vo.SeatWithReserved;
 import finalmission.seat.service.SeatFrontService;
 import finalmission.venue.controller.dto.VenueRequest;
 import finalmission.venue.controller.dto.VenueResponse;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
@@ -31,6 +33,11 @@ public class SeatController {
     @GetMapping("/{id}")
     public ResponseEntity<SeatResponse> getSeat(@PathVariable final Long id) {
         return ResponseEntity.ok(seatFrontService.get(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<SeatWithReserved>> getSeats(@RequestParam final Long concertId) {
+        return ResponseEntity.ok(seatFrontService.getSeatsWithReserved(concertId));
     }
 
     @GetMapping
