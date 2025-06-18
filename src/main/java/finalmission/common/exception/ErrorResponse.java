6ceac5ad09af64f49f890670339fb1935b7c7400
egaeng09 <egaeng09@gmail.com>
2025-06-18 20:55:28 +0900

    refactor: 상태 코드 타입 변경

--- src/main/java/finalmission/common/exception/ErrorResponse.java
+++ src/main/java/finalmission/common/exception/ErrorResponse.java
@@ -1,28 +1,20 @@
 package finalmission.common.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.http.HttpStatus;
 
-public class ErrorResponse {
-
-    private final String method;
-    private final String uri;
-    private final HttpStatus status;
-    private final String message;
-
-    public ErrorResponse(final String method, final String uri, final HttpStatus status, final String message) {
-        this.method = method;
-        this.uri = uri;
-        this.status = status;
-        this.message = message;
-    }
+public record ErrorResponse(
+        String method,
+        String uri,
+        int status,
+        String message
+) {
 
     public static ErrorResponse of(final HttpServletRequest request, final ErrorCode errorCode) {
         return new ErrorResponse(
-            request.getMethod(),
-            request.getRequestURI(),
-            errorCode.getStatus(),
-            errorCode.getMessage()
+                request.getMethod(),
+                request.getRequestURI(),
+                errorCode.getStatus().value(),
+                errorCode.getMessage()
         );
     }
 }
