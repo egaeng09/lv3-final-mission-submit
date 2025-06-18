a6812ab0be9115e95ff29e221b74441e9f2aacf1
egaeng09 <egaeng09@gmail.com>
2025-06-18 23:10:56 +0900

    test: 누락된 에노테이션 추가

--- src/test/java/finalmission/reservation/service/ReservationFrontServiceTest.java
+++ src/test/java/finalmission/reservation/service/ReservationFrontServiceTest.java
@@ -27,12 +27,14 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.web.client.RestClient;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 class ReservationFrontServiceTest {
 
     private ReservationFrontService reservationFrontService;
